590faa0d18a3a74528d94cf5502d5049e51b5908
greitavi000 <greitavi000@CAM-LAB-16.bgsd.k12.wa.us>
2019-09-19 07:37:21 -0700

    LettersDone.java

--- src/Letters.java
+++ src/Letters.java
@@ -3,29 +3,29 @@ public class Letters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Grandma();
-		Sister();
-		Friend();
+		grandma();
+		sister();
+		friend();
 	}
 	
-		public static void Grandma() {
+		public static void grandma() {
 			System.out.println(" \" Hi, Grandma it's Avi.");
-			SameToAll();
+			sameToAll();
 			
 		}
-		public static void Sister() {
+		public static void sister() {
 			System.out.println(" \" Hi, sister it's ur sister.");
-			SameToAll();
+			sameToAll();
 		
 		}
-		public static void Friend() {
+		public static void friend() {
 			System.out.println(" \" Hi, Masha it's Avi.");
-			SameToAll();
+			sameToAll();
 			
 		}
 		
 	
-		public static void SameToAll() {
+		public static void sameToAll() {
 			System.out.println(" Hope you having a grate day.");
 			System.out.println(" I want to invite you to my B-day family party.");
 			System.out.println(" It will be at 11:30am in our living room.");
